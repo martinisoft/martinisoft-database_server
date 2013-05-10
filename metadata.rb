3a68b2bf60f004ca55270ccff15908ee734e04b2
Aaron Kalin <akalin@martinisoftware.com>
2013-05-10 00:12:01 -0500

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             "martinisoft-database_server"
+maintainer       "Aaron Kalin"
+maintainer_email "akalin@martinisoftware.com"
+license          "Apache 2.0"
+description      "Database server role cookbook for Martini Software"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
+
